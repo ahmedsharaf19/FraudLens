18cc4328af9239d296caecb1bc39a457bc5813c5
ahmedsharaf19 <ahmed.sharaf.connect@gmail.com>
2026-01-03 21:57:13 +0200

    Fix Keyinterupt Exception

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from app import ConsoleApp
+from constants import SPACE
 import os
 
 if __name__ == "__main__":
-    app = ConsoleApp()
-    app.main_menu()
+    try :
+        app = ConsoleApp()
+        app.main_menu()
+    except:
+        print(f"{SPACE}👋 Exiting FRAUDLENS ...")
